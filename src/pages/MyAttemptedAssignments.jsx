1aaf12ba195bda1e2bea20b4b93fbc0f740de773
shRahat10 <shahriar.haque.1011@gmail.com>
2024-05-12 19:13:02 +0600

    my attempts

--- src/pages/MyAttemptedAssignments.jsx
+++ src/pages/MyAttemptedAssignments.jsx
@@ -1,5 +1,6 @@
 import { useContext } from "react";
 import { AuthContext } from "../provider/AuthProvider";
+import { GrDocumentPdf } from "react-icons/gr";
 
 
 const MyAttemptedAssignments = () => {
@@ -11,26 +12,28 @@ const MyAttemptedAssignments = () => {
         <>
             <h1 className=" text-center font-bold text-2xl mb-6">My Assignments</h1>
 
-            <div className=" grid grid-cols-6 gap-2 font-bold border-b-2 text-xs md:text-base">
-                <p className=" py-2 col-span-2">Title</p>
-                <p className=" py-2">Status</p>
-                <p className=" py-2">Marks</p>
-                <p className=" py-2">Obtained</p>
-                <p className=" py-2">Feedback</p>
+            <div className=" grid grid-cols-12 gap-2 font-bold border-b-2 text-xs md:text-base">
+                <div></div>
+                <p className=" py-2 col-span-3">Title</p>
+                <p className=" py-2 col-span-2">Status</p>
+                <p className=" py-2 col-span-2">Marks</p>
+                <p className=" py-2 col-span-2">Obtained</p>
+                <p className=" py-2 col-span-2">Feedback</p>
             </div>
             {
                 filteredData?.map((e, idx) => (
-                    <div key={idx} className=" grid grid-cols-6 gap-2 border-b text-xs md:text-base">
-                        <p className=" py-2 col-span-2">{e.title}</p>
+                    <div key={idx} className=" grid grid-cols-12 gap-2 border-b text-xs md:text-base">
+                        <div className="py-2 text-red-600"><GrDocumentPdf size={20}/></div>
+                        <p className=" py-2 col-span-3">{e.title}</p>
                         {
-                            e.status === 'pending' && <p className="py-2 text-orange-500">{e.status}</p>
+                            e.status === 'pending' && <p className="py-2 col-span-2 text-orange-500">{e.status}</p>
                         }
                         {
-                            e.status === 'completed' && <p className="py-2 text-green-500">{e.status}</p>
+                            e.status === 'completed' && <p className="py-2 col-span-2 text-green-500">{e.status}</p>
                         }
-                        <p className=" py-2">{e.totalMarks}</p>
-                        <p className=" py-2">{e.obtainedMarks}</p>
-                        <p className=" py-2">{e.feedback}</p>
+                        <p className=" py-2 col-span-2">{e.totalMarks}</p>
+                        <p className=" py-2 col-span-2">{e.obtainedMarks}</p>
+                        <p className=" py-2 col-span-2">{e.feedback}</p>
                     </div>
                 ))
             }
