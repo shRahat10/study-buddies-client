97ab22bcfa394545c06610ba62f4ef30c398d19a
Shahriar Haque <shahriarhaquerahat10@gmail.com>
2024-05-13 18:15:43 +0600

    faq correction

--- src/pages/Home/Faq.jsx
+++ src/pages/Home/Faq.jsx
@@ -11,7 +11,7 @@ const Faq = () => {
     const { View } = useLottie(options);
 
     return (
-        <div className=" grid grid-cols-1 md:grid-cols-5 items-center">
+        <div className=" grid grid-cols-1 md:grid-cols-5 items-center dark:text-white">
             <div className=" col-span-2">{View}</div>
             <div className="join join-vertical w-full col-span-3">
                 <div className="collapse collapse-arrow join-item border border-base-300">
@@ -20,7 +20,7 @@ const Faq = () => {
                         How can I join Study Buddies?
                     </div>
                     <div className="collapse-content">
-                        <p>To join Study Buddies, simply click on the "Sign Up" button at the top right corner of the page. Fill out the required information, and you'll become a member of our community instantly! Once you're signed up, you can explore all the features and resources available to help you excel in your studies.</p>
+                        <p>To join Study Buddies, simply click on the Sign Up button at the top right corner of the page. Fill out the required information, and you will become a member of our community instantly! Once you are signed up, you can explore all the features and resources available to help you excel in your studies.</p>
                     </div>
                 </div>
                 <div className="collapse collapse-arrow join-item border border-base-300">
@@ -29,7 +29,7 @@ const Faq = () => {
                         What kind of study materials does Study Buddies offer?
                     </div>
                     <div className="collapse-content">
-                        <p>Study Buddies offers a wide range of study materials to cater to different subjects and academic levels. From lecture notes and practice quizzes to study guides and interactive tutorials, you'll find everything you need to enhance your learning experience. Our study materials are curated by experts and designed to help you understand complex concepts easily.</p>
+                        <p>Study Buddies offers a wide range of study materials to cater to different subjects and academic levels. From lecture notes and practice quizzes to study guides and interactive tutorials, you will find everything you need to enhance your learning experience. Our study materials are curated by experts and designed to help you understand complex concepts easily.</p>
                     </div>
                 </div>
                 <div className="collapse collapse-arrow join-item border border-base-300">
