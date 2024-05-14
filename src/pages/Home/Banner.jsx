eb47166a1f021841946018c8a7a726d5fd6e7211
shRahat10 <shahriar.haque.1011@gmail.com>
2024-05-14 19:38:06 +0600

    final

--- src/pages/Home/Banner.jsx
+++ src/pages/Home/Banner.jsx
@@ -9,6 +9,7 @@ import 'swiper/css/autoplay';
 import banner1 from "../../assets/images/banner1.jpg"
 import banner2 from "../../assets/images/banner2.jpg"
 import banner3 from "../../assets/images/banner3.jpg"
+import { Cursor, useTypewriter } from 'react-simple-typewriter'
 
 const Banner = () => {
     const options = {
@@ -19,6 +20,38 @@ const Banner = () => {
 
     const { View } = useLottie(options);
 
+    const [head1] = useTypewriter({
+        words: ['Welcome to Study Buddies'],
+        loop: true,
+        onLoopDone: () => console.log(`loop completed`)
+    })
+    const [body1] = useTypewriter({
+        words: ['The beautiful journey of today can only begin when we learn to let go of yesterday.'],
+        loop: true,
+        onLoopDone: () => console.log(`loop completed`)
+    })
+    const [head2] = useTypewriter({
+        words: ['Unlock Your Potential'],
+        loop: true,
+        onLoopDone: () => console.log(`loop completed`)
+    })
+    const [body2] = useTypewriter({
+        words: ['The only way to do great work is to love what you do.'],
+        loop: true,
+        onLoopDone: () => console.log(`loop completed`)
+    })
+    const [head3] = useTypewriter({
+        words: ['Discover Your Brilliance'],
+        loop: true,
+        onLoopDone: () => console.log(`loop completed`)
+    })
+    const [body3] = useTypewriter({
+        words: ['Education is the passport to the future, for tomorrow belongs to those who prepare for it today.'],
+        loop: true,
+        onLoopDone: () => console.log(`loop completed`)
+    })
+
+
     return (
         <div className="grid grid-cols-1 md:grid-cols-5 justify-center items-center">
             <div className="col-span-3 order-2 md:order-1">
@@ -27,25 +60,43 @@ const Banner = () => {
                     spaceBetween={50}
                     slidesPerView={1}
                     navigation
-                    autoplay={{ delay: 3000 }}
+                    autoplay={{ delay: 10000 }}
                     pagination={{ clickable: true }}
                 >
                     <SwiperSlide style={{ backgroundImage: `url(${banner1})`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
                         <div className="bg-gray-900 bg-opacity-50 p-10 rounded h-80 flex flex-col justify-center">
-                            <h2 className="text-white text-2xl font-bold mb-6">Welcome to Study Buddies</h2>
-                            <p className="text-white text-lg">The beautiful journey of today can only begin when we learn to let go of yesterday.</p>
+                            <div className='App h-10'>
+                                <span className="text-white text-2xl font-bold mb-6">{head1}</span>
+                                <Cursor cursorColor='red' />
+                            </div>
+                            <div className='App h-16'>
+                                <span className="text-white text-lg">{body1}</span>
+                                <Cursor cursorColor='red' />
+                            </div>
                         </div>
                     </SwiperSlide>
                     <SwiperSlide style={{ backgroundImage: `url(${banner2})`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
                         <div className="bg-gray-900 bg-opacity-50 p-10 rounded h-80 flex flex-col justify-center">
-                            <h2 className="text-white text-2xl font-bold mb-6">Unlock Your Potential</h2>
-                            <p className="text-white text-lg">The only way to do great work is to love what you do.</p>
+                            <div className='App h-10'>
+                                <span className="text-white text-2xl font-bold mb-6">{head2}</span>
+                                <Cursor cursorColor='red' />
+                            </div>
+                            <div className='App h-16'>
+                                <span className="text-white text-lg">{body2}</span>
+                                <Cursor cursorColor='red' />
+                            </div>
                         </div>
                     </SwiperSlide>
                     <SwiperSlide style={{ backgroundImage: `url(${banner3})`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
                         <div className="bg-gray-900 bg-opacity-50 p-10 rounded h-80 flex flex-col justify-center">
-                            <h2 className="text-white text-2xl font-bold mb-6">Discover Your Brilliance</h2>
-                            <p className="text-white text-lg">Education is the passport to the future, for tomorrow belongs to those who prepare for it today.</p>
+                            <div className='App h-10'>
+                                <span className="text-white text-2xl font-bold mb-6">{head3}</span>
+                                <Cursor cursorColor='red' />
+                            </div>
+                            <div className='App h-16'>
+                                <span className="text-white text-lg">{body3}</span>
+                                <Cursor cursorColor='red' />
+                            </div>
                         </div>
                     </SwiperSlide>
                 </Swiper>
