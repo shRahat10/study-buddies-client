dfa1c79601dde43bba17f6486ad9b34401923f46
Shahriar Haque <shahriarhaquerahat10@gmail.com>
2024-05-15 18:08:34 +0600

    final

--- src/pages/AssignmentSubmissionForm .jsx
+++ src/pages/AssignmentSubmissionForm .jsx
@@ -25,6 +25,7 @@ const AssignmentSubmissionForm = ({ onClose, data, user }) => {
             headers: {
                 'Content-Type': 'application/json'
             },
+            credentials: 'include',
             body: JSON.stringify(e)
         })
         .then(res => res.json())
